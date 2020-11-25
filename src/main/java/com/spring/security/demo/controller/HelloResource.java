5bf21cfed1c6a1091b9002e5c232076036bdc705
Hasara <hasarasamson97@gmail.com>
2020-11-25 17:30:53 +0530

    Basic authentication

--- src/main/java/com/spring/security/demo/controller/HelloResource.java
+++ src/main/java/com/spring/security/demo/controller/HelloResource.java
@@ -1,16 +1,13 @@
 package com.spring.security.demo.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping("/hello")
+@RestController
 public class HelloResource {
 
-
-    @GetMapping
-    public String hello(){
+    @RequestMapping("/hello")
+    public String hello() {
         return "Hello, Spring Security";
     }
 }
